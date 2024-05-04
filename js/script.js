729882ba4becc024c24ac8a662724aa932fc876c
aysun013 <ismailovaaysuna28@gmail.com>
2024-05-04 05:07:40 +0400

    js task

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// task 1:
+
+// let x = prompt('How old are you?');
+
+// if (x>20) {
+//     alert('yasiniz uygundur')
+// }
+// else if(x<18) {
+//     alert('yasiniz azdir')
+// }else(
+//     alert('hec biri')
+// )
+
+
+
+// task 2:
+
+// let n =0
+
+// while (n<3) {
+//     alert(n)
+// n++
+    
+// };
+
+
+// task 3:
+
+// let a = prompt('is saatlariniz ne zamandir?');
+
+// if(9 < a && a < 18) {
+// alert('is saatlari doğrudur')
+// }
+//  else{
+//    alert('is saatlari yanlisdir')
+// } 
+
+// let i =1
+
+// for (let i = 0; i < 3; i++) { // shows 0, then 1, then 2
+//     console.log(i + " forun icindeki");
+//   }
+
+//   console.log(i);
+
+
+
+
+
+
+
+
+
+
