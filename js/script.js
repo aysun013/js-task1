339f8a2a4261901ec123994c1aa7604c45cee694
aysun013 <ismailovaaysuna28@gmail.com>
2024-05-11 16:26:16 +0400

    declared function

--- js/script.js
+++ js/script.js
@@ -58,14 +58,116 @@
 // }
 // console.log(sum[1]);
 
-let arr = [];
-for (let i = 10; i>=1; i--){
-   arr[arr.length]=i;
+
+// task 4
+
+
+// function Salam() {
+//   console.log('salam');  
+// }
+
+// Salam() invoked
+
+
+
+
+
+// function Sum(x,y) {
+
+//    let a = x * 2
+//    let b = y + 10
+//     console.log(a,b);
+// }
+
+// Sum(3,5)
+
+// let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let arr2 = [];
+
+// for (let i = a.length - 1; i >= 0; i--) {
+//     arr2[arr2.length] = a[i];
+    
+
+// }
+
+// console.log(arr2);
+// (function Together() {
+    
+    //     console.log(true);
+    
+    // })()
+    // self-invoked function
+    
+ 
+    // function Fokus(a) {
+    //     console.log(a);
+    // }
+    // Fokus('salam')
+
+// let h2 = document.querySelector('.Aysun')
+
+// function declaration
+// function salam(){
+//     console.log('salam');
+// }
+
+// function Niye(b) {
+   
+    
+//     return (
+    
+//     )
+
+
+// }
+
+
+// h2.innerHTML = Niye(2)
+
+// console.log(Niye(3));
+
+// salam()
+
+// let a = function(){
+//     return 'Sevda'
+// }
+
+// console.log(a());
+
+
+// var a = 'Aysun'
+
+// function Fulname() {
+//    var a;
+//     console.log(a);
+// }
+
+// Fulname()
+
+
+// var z = "car"
+
+// if (true) {
+//     var z;
+//     console.log(z);
+// }
+
+// console.log(z);
+
+let name = 'soul'
+// let h1=document.querySelector('.a')
+
+function lalala() {
+let name = 'inside out'
+
+let message = 'animation ' + name;
+console.log(message);
+// return()
+  
 }
-console.log(arr);
-
-
-
+// h1.innerHTML = lalala()
+lalala()
+console.log(name);
 
 
 
